b0eaa65de018cb38180e12a40898b19c8968bf62
EdwinSanjeev <94860839+edwinsanjeev@users.noreply.github.com>
2022-01-19 17:33:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Programmer: Edwin 
+# Date: 1/19/2022
+# Description: Exercise 13
+
+santas_reindeer = [
+    "Rudolph",
+    "Dasher",
+    "Dancer",
+    "Prancer",
+    "Vixen",
+    "Comet",
+    "Cupid",
+    "Donder",
+    "Blitzen"
+]
+
+print("Santa's Reindeer are:")
+
+for santas_reindeer_1 in santas_reindeer:
+    print(santas_reindeer_1)
